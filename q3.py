49d6d18146d8d7c1457f50a906bcae1b22c14e0b
Jake Gadaleta <jake.gads@gmail.com>
2020-10-23 23:34:51 -0400

    gamer

--- q3.py
+++ q3.py
@@ -1,5 +1,18 @@
 def main(int1, int2):
-   return "Your return here"
+    low = int1
+
+    if int2 < int1:
+        low = int2
+
+    for i in list(range(low)).__reversed__():
+        i += 1
+
+        if int1 % i == 0 and int2 % i == 0:
+            int1 /= i
+            int2 /= i
+            
+
+    return "The reduced fraction is: {}/{}".format(int(int1), int(int2))
 
 if __name__ == "__main__":
     print(
